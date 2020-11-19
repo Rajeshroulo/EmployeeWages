5c7c2d7366cdf703625234dff9f826060c73c724
Rajeshroulo <rajraval017@gmail.com>
2020-11-19 15:38:14 +0530

    Added feature to check employee attendance

--- employeeWage.php
+++ employeeWage.php
@@ -6,5 +6,20 @@ class EmployeeWage
 		echo "\n Welcome to Employee Wage Computation Program \n";
 	  }
 
-	}	  
+	  public function checkAttendance(){
+		$empCheck= rand(0,1);
+		if($empCheck==1)
+		{
+			echo"\n employee is present";		  
+		}
+  
+		else
+		{
+		  echo"\n employee is absent";		    
+		}
+	  }
+		  
+   }
+   $emp = new EmployeeWage();
+	  $emp->checkattendance();	  
 ?>	
